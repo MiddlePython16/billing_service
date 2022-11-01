d8822e897a1fd198a4e860ef37ce201924c7263e
Emir Takhaviev <58186925+Wiped-Out@users.noreply.github.com>
2022-11-02 00:37:09 +0300

    Fix imports in apps

--- billing_compose/django/src/payment/apps.py
+++ billing_compose/django/src/payment/apps.py
@@ -1,11 +1,10 @@
 from django.apps import AppConfig
 
-from . import kafka
-
 
 class PaymentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'payment'
 
     def ready(self):
+        from . import kafka, receivers  # noqa
         kafka.kafka_producer = kafka.init_kafka_producer()
