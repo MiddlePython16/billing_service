dacccbfd27babfe30393a39a0cfbba203fa22a8f
Emir Takhaviev <58186925+Wiped-Out@users.noreply.github.com>
2022-11-01 16:35:17 +0300

    Fix №7 from review

--- billing_compose/django/src/extensions/jwt/utils.py
+++ billing_compose/django/src/extensions/jwt/utils.py
@@ -3,8 +3,6 @@ import time
 import jwt
 from config import settings
 
-EXPIRE_TIME_IN_SECONDS = 600
-
 
 def decode_jwt(token: str) -> dict:
     try:
@@ -21,6 +19,6 @@ def decode_jwt(token: str) -> dict:
             options={'verify_signature': False},
         )
         time_now = int(time.time())
-        if time_now - decoded['exp'] < EXPIRE_TIME_IN_SECONDS:
+        if time_now - decoded['exp'] < settings.AUTH_TOKEN_EXPIRE_IN_SECONDS:
             return decoded
         raise error
